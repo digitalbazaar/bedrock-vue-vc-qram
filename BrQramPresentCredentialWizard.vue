78e49d08d7ee86a9c22db8a6241159aef21f5940
Dave Longley <dlongley@digitalbazaar.com>
2019-08-27 15:46:11 -0400

    Add core files.

---
+++ BrQramPresentCredentialWizard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+/*!
+  * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+  */
+'use strict';
+
+export default {
+  name: 'BrQramPresentCredentialWizard',
+  props: {
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
